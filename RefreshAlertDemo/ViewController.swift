4ea84f6ba68e01c6c4673a7838d8f1f213fd07e7
Muhammad Doukmak <mkdoukmak@gmail.com>
2021-10-20 13:13:25 -0500

    Simulate pull to refresh issue

--- RefreshAlertDemo/ViewController.swift
+++ RefreshAlertDemo/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UITableViewController {
 
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableView.automaticDimension;
+        tableView.estimatedRowHeight = 44.0; // set to whatever your "average" cell height is
+        self.refreshControl = UIRefreshControl()
+        self.refreshControl?.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        refresh()
+    }
+    
+    @objc private func refresh() {
+        refreshControl?.beginRefreshing()
+        Loader.fetch(completion: { [weak self] result in
+            self?.refreshControl?.endRefreshing()
+            switch result {
+            case .success(_):
+                break
+            case .failure(let error):
+//                print("error \(error.localizedDescription)")
+                let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel))
+                self?.show(alert, sender: self)
+            }
+        })
     }
 
+}
 
+public class Loader {
+    public static func fetch(completion: @escaping (Result<Int, Error>) -> ()) {
+        let error = NSError(domain: "Domain", code: 0)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+            completion(.failure(error))
+        }
+    }
 }
 
